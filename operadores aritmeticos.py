80e5926c3237a17f30d18d800cb6558e3739b8e8
Samanthaidk9 <ksva745@gmail.com>
2023-02-28 21:42:31 -0500

    operadores.

---
+++ operadores aritmeticos.py
@@ -0,0 +1,22 @@
+#libreria
+import math
+##Adquirir informacion
+##Entradas
+a = int (input("digite el numero 1:"))
+print (f"El numero a que digito es: {a}")
+b = int(input("digite el numero 2:"))
+print (b)
+#proceso
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = a**b
+h = math.sqrt (a)
+#Salida
+print(f"el resultado de la suma es: {c}")
+print(f"el resultado de la resta es: {d}")
+print(f"el resultado de la multiplicacion es: {e}")
+print(f"el resultado de la division es: {f}")
+print(f"el resultado de la potencia es: {g}")
+print(f"el resultado de la raiz es: {h}")
---
+++ readmi.md
@@ -0,0 +1,4 @@
+
+
+- Aqui se encontraran apuntes y todo lo correspondiente a la asignatura de analisis de algoritmos del primer semestre del año 2023.
+![](https://www.undefinedworld.com/assets/images/articles/analisis-de-algoritmos-1200x630.jpg)
